fcd89f6be3ce87d583d26db719c2b086ebb539e2
Trần Hải Nam <steven.hai.ta856@gmail.com>
2024-06-03 03:09:50 +0700

    second commit

---
+++ src/test/java/com/eazybytes/springdeploy/SpringDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
