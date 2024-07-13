1b7bf99f29cc75c244f008f7252a22ea7e0220b2
Trần Hải Nam <steven.hai.ta856@gmail.com>
2024-07-14 04:12:06 +0700

    fix deploy

--- src/main/java/com/ecommerce/server/SpringDeployApplication.java
+++ src/main/java/com/ecommerce/server/SpringDeployApplication.java
@@ -13,36 +13,7 @@ import java.nio.file.attribute.BasicFileAttributes;
 @EnableJpaRepositories(basePackages = "com.ecommerce.server.repository")
 public class SpringDeployApplication {
 
-    public static void main(String[] args) throws IOException {
-        String rootDir = System.getProperty("user.dir"); // Replace with your root directory path
-        String fileNameToFind = ".env"; // Replace with the file name you want to find
-        final boolean[] found = {false};
-        Files.walkFileTree(Path.of(rootDir), new SimpleFileVisitor<Path>() {
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                if (file.getFileName().toString().equals(fileNameToFind)) {
-                    System.out.println("File found: " + file.toAbsolutePath());
-                    found[0] = true;
-                    return FileVisitResult.TERMINATE; // Terminate search after finding the first occurrence
-                }
-                else {
-                    System.out.println(file.getFileName().toString());
-                }
-                return FileVisitResult.CONTINUE; // Continue searching
-            }
-
-            @Override
-            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-                return FileVisitResult.CONTINUE; // Continue if access is denied or other errors occur
-            }
-        });
-
-        if(found[0]) {
-            System.out.println("Found file .env");
-        }
-        else {
-            System.out.println(".env not found");
-        }
+    public static void main(String[] args)  {
         SpringApplication.run(SpringDeployApplication.class, args);
     }
 
