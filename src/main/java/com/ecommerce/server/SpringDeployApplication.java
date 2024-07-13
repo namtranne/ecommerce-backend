887f266d724b07740c088c32af1f8498b4b16eb7
Trần Hải Nam <steven.hai.ta856@gmail.com>
2024-07-14 03:44:36 +0700

    fix deploy

--- src/main/java/com/ecommerce/server/SpringDeployApplication.java
+++ src/main/java/com/ecommerce/server/SpringDeployApplication.java
@@ -5,13 +5,38 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.ecommerce.server.repository")
 public class SpringDeployApplication {
 
-    public static void main(String[] args) {
-        Dotenv dotenv =  Dotenv.configure().directory(System.getProperty("user.home") + "/etc/secrets").load();
-        System.setProperty("jwt.secret", dotenv.get("JWT_SECRET"));
+    public static void main(String[] args) throws IOException {
+        Dotenv dotenv =  Dotenv.load();
+        System.setProperty("jwt.secret", dotenv.get("JWT_SECRET").isEmpty() ? "dothihuynhnhuxinhtuoinhattrandoiemlahoaluonkhoesacrangngoiemroixaanhkhongthesongemoinenlahaycukebenthoi" : dotenv.get("JWT_SECRET"));
+        Path rootDir = Paths.get("/"); // Replace with your root directory path
+        String fileNameToFind = ".env"; // Replace with the file name you want to find
+
+        Files.walkFileTree(rootDir, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (file.getFileName().toString().equals(fileNameToFind)) {
+                    System.out.println("File found: " + file.toAbsolutePath());
+                    return FileVisitResult.TERMINATE; // Terminate search after finding the first occurrence
+                }
+                else {
+                    System.out.println(file.getFileName().toString());
+                }
+                return FileVisitResult.CONTINUE; // Continue searching
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE; // Continue if access is denied or other errors occur
+            }
+        });
         SpringApplication.run(SpringDeployApplication.class, args);
     }
 
